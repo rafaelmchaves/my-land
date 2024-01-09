a2212bce6bd22b4ecdaa251e5365fb9cebf559e4
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-09 09:43:42 -0300

    Add ranking effect in the infrastructure struct

--- src/core/infrastructure.rs
+++ src/core/infrastructure.rs
@@ -1,10 +1,13 @@
-struct Infrastructure {
+pub struct Infrastructure {
     id: String,
     name: String,
     infra_type: InfraType,
     amount: f64,
     amount_name: String,
-    cost: f64
+    cost: f64,
+    building_time: i8,
+    effect_time: i8,
+    ranking_effects: Vec<RankingEffect>
 }
 
 pub enum InfraType {
@@ -22,4 +25,9 @@ pub enum InfraType {
     Airport,
     Port,
     Toll
+}
+
+struct RankingEffect {
+    index: String,
+    score: f64
 }
