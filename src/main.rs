e6c9a363fd5d8af4a0aa8c441a02814941aff85c
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-09 15:48:52 -0300

    Test the use of files to configure the project

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-pub mod core;
+use crate::repository::file::{read_file, write_file};
 
+mod repository;
 fn main() {
     println!("Hello, world!");
+
+    let object = read_file("infra.json");
+
+    write_file(object.unwrap());
+    
 }
