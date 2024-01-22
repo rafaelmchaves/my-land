1bb7647cc2bcc8dbd5e2e78da00100206d6c70ca
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-22 19:59:35 -0300

    Add TODO tasks in the generate next turn

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,12 @@
 pub fn generate_next_turn() {
+
+    // TODO call generate_population method
+    // TODO call generate_complainings
+    // TODO call generate_news
+    // TODO Check if an infrastructure is completed, and show in the next turn as ready. Add this infrastructure in the ready list, and start to count the effect time.
+    // TODO Check if an infrastructure reached the effect time, and change the score.
+    // TODO Recalculate the ranking of each index.
+    // TODO Calculate the incomes and expenses.
+
     println!("advance to the next turn")
 }
