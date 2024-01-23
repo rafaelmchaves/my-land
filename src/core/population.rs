168f6b4d963d115cf52b263e838acb6a478e4735
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-23 12:02:36 -0300

    Add the person struct and the generate_population method

---
+++ src/core/population.rs
@@ -0,0 +1,18 @@
+//file that it will have all population logical rules such as complain generation, population generate, demography and many others.
+
+struct Person {
+    id: String,
+    birthdate: String,
+    name: String,
+    gender: String
+}
+
+
+pub fn generate_population() {
+
+    //TODO get the birthrate and mortality rate.
+    //TODO verify if there is a policy about the controll of birth
+    //TODO apply the calculation of birthrate and mortality rate.
+    //TODO return an object with the new habitants and who was dead. In addition to it, the new demograph
+
+}
