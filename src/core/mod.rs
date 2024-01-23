e9c62f33367b2a3a57b331c75b5388f0c5e24395
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-23 12:12:02 -0300

    Calling memory data from the game action

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -14,6 +14,6 @@ pub fn generate_next_turn(memory_data: MemoryData) {
     println!("advance to the next turn")
 }
 
-struct MemoryData {
+pub struct MemoryData {
 
 }
--- src/ui/menu.rs
+++ src/ui/menu.rs
@@ -126,7 +126,7 @@ mod splash {
 mod game {
     use bevy::prelude::*;
 
-    use crate::core::generate_next_turn;
+    use crate::core::{generate_next_turn, MemoryData};
 
     use super::{despawn_screen, GameState, TEXT_COLOR};
 
@@ -164,7 +164,7 @@ mod game {
         for (interaction, menu_button_action) in &interaction_query {
             if *interaction == Interaction::Pressed {
                 match menu_button_action {
-                    ButtonEventsAction::Advance => {generate_next_turn()},
+                    ButtonEventsAction::Advance => {generate_next_turn(MemoryData {  })},
                     ButtonEventsAction::BackMenu => {
                         game_state.set(GameState::Menu);
                     }
