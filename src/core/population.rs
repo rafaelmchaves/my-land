506962a3520ebff3f5ae351b9271938f96e9e1cc
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-24 16:27:15 -0300

    Changing some rules of the system

--- src/core/population.rs
+++ src/core/population.rs
@@ -17,10 +17,13 @@ struct Complaint {
     date: String
 }
 
+struct Demography {
+    
+}
+
 pub fn generate_population(memory_data: &MemoryData) {
 
     //TODO get the birthrate and mortality rate.
-    //TODO verify if there is a policy about the controll of birth
     //TODO apply the calculation of birthrate and mortality rate.
     //TODO return an object with the new habitants and who was dead. In addition to it, the new demograph
 
