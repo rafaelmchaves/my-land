5b5d7b0387c134c09a5f75734553f4a8734ea26f
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-26 15:15:10 -0300

    adding the stdin to get what the user is typing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,25 @@
 use crate::repository::{policy, ranking};
 use crate::ui::open;
+use std::io;
 
 mod repository;
 mod ui;
 mod core;
 
-fn main() {
+fn main() -> io::Result<()> {
 
-    policy::get_all_policies();
+    let mut command:String = String::new();
+    while command!= "-1".to_string() {
     
-    ranking::get_ranking_by_name_and_year("health", "1880");
+        println!("Type a number");
+        command = std::io::stdin().lines().next().unwrap().unwrap();
+       
+        println!("command: {}", &command);
+        policy::get_all_policies();
+        
+        ranking::get_ranking_by_name_and_year("health", "1880");
 
-    open();
+    // open();
+    }
+    Ok(())
 }
