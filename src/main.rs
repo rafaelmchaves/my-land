1296d78bec37f49675f91a73cc4609a480feaed8
Rafael Marques Chaves <rafaelmchaves@gmail.com>
2024-01-29 21:41:37 -0300

    Associate the infra id with the selected option.
    Build the selected infrastructure.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use repository::infrastructure::Infrastructure;
+
 use crate::repository::{policy, ranking, infrastructure};
 use crate::ui::open;
 use std::io;
@@ -43,24 +45,65 @@ fn build_infrastructure_options(option: &mut String) {
     let infra_result = infrastructure::get_all_infrastructures();
     if infra_result.is_ok() {
         let infra_list = infra_result.unwrap();
-    
-        println!("Select one of the options below:");
-        for item in infra_list {
-            println!("{} - Build {}", item.id, item.name);
+  
+        while option.as_str() != "-1" && option.as_str() != "y" {
+            println!("Select one of the options below:");
+            for item in &infra_list {
+                println!("{} - Build {}", item.id, item.name);
+            }
+            
+            println!("-1 - Return to the previous menu");
+            *option = std::io::stdin().lines().next().unwrap().unwrap();
+            if option.as_str() != "-1" {
+
+                
+                let infra_selected = select_infrastructure(&infra_list, option);
+
+                if (&infra_selected).is_some() {
+                
+                    let mut option_confirmation:String = String::new();
+                    create_confirmation_selected(&infra_selected, &mut option_confirmation);
+                    if option_confirmation.as_str() == "y" {
+                        *option = "y".to_string();
+                        println!("Item has built")
+                    }
+                    
+                } else {
+                    println!("Select a valid option");
+                }
+                
+            } 
         }
         
-        println!("-1 - Return to the previous menu");
-        *option = std::io::stdin().lines().next().unwrap().unwrap();
-        match option.as_str() {
-            "1" => build_policy_x(),
-            _ => println!("returning to menu")
-        }
     } else {
         println!("It was not found any infrastructure. Try again");
     }
 
     *option = "0".to_string();
 
+}
+
+fn create_confirmation_selected(infra_selected: &Option<Infrastructure>, option: &mut String) {
+    let item = (infra_selected.clone()).unwrap();
+    println!("-----------------------------------------");
+    println!("The element that you want to build is: ");
+    println!("Name: {}", item.name);
+    println!("Price: ${}", item.cost);
+    println!("Time to build: {} weeks", item.building_time);
+    println!("Time to take effect after built: {} weeks", item.effect_time);
+    println!("Monthly cost: ${} ", item.monthly_cost);
+
+    println!("Do you want to build? y/n ");
+    *option = std::io::stdin().lines().next().unwrap().unwrap();
+}
+
+fn select_infrastructure(infra_list: &Vec<Infrastructure>, option: &mut String) -> Option<Infrastructure> {
+    for item in infra_list {
+        if item.id == option.to_string() {
+            return Some(item.clone())
+        }
+    }
+    None
 }
 
 fn build_policy_options(option: &mut String) {
